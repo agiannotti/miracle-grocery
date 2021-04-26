51d972215a09b83ed51441a050bd153d1eab6b8d
tonyg <aj.gianno@gmail.com>
2021-04-26 17:39:13 -0500

    firebase init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+import { environment } from '../environments/environment';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAq07eCa1O9xv-3s_KTvIt_IcXlbBMvcp0',
+    authDomain: 'miracle-grocery.firebaseapp.com',
+    databaseURL: 'https://miracle-grocery-default-rtdb.firebaseio.com',
+    projectId: 'miracle-grocery',
+    storageBucket: 'miracle-grocery.appspot.com',
+    messagingSenderId: '680935477338',
+    appId: '1:680935477338:web:66272c57eabc4a842a0863',
+    measurementId: 'G-3WPG795KEL',
+  },
 };
