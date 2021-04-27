509ca3f317ef415d7c8dd3d13cbbc9ff49b19f7b
tonyg <aj.gianno@gmail.com>
2021-04-26 19:44:39 -0500

    app routing updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,56 +1,23 @@
 import { NgModule } from '@angular/core';
 import { environment } from '../environments/environment';
-[];
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireDatabaseModule } from '@angular/fire/database';
 import { AngularFireAuthModule } from '@angular/fire/auth';
-import { RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-import { AppComponent } from './app.component';
-import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
-import { HomeComponent } from './home/home.component';
-import { ProductsComponent } from './products/products.component';
-import { CheckoutComponent } from './checkout/checkout.component';
-import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
-import { OrderSuccessComponent } from './order-success/order-success.component';
-import { MyOrdersComponent } from './my-orders/my-orders.component';
-import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
-import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
-import { LoginComponent } from './login/login.component';
-
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    BsNavbarComponent,
-    ProductsComponent,
-    CheckoutComponent,
-    ShoppingCartComponent,
-    OrderSuccessComponent,
-    MyOrdersComponent,
-    AdminProductsComponent,
-    AdminOrdersComponent,
-    LoginComponent,
-  ],
+  declarations: [],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
     AngularFireAuthModule,
     NgbModule,
-    RouterModule.forRoot([
-      { path: '', component: HomeComponent },
-      { path: 'products', component: ProductsComponent },
-      { path: 'shopping-cart', component: ShoppingCartComponent },
-      { path: 'checkout', component: CheckoutComponent },
-      { path: 'order-success', component: OrderSuccessComponent },
-      { path: 'login', component: LoginComponent },
-      { path: 'admin/admin-products', component: AdminProductsComponent },
-      { path: 'admin/admin-orders', component: AdminOrdersComponent },
-    ]),
   ],
   providers: [],
   bootstrap: [AppComponent],
