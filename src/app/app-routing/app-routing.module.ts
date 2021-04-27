51db9ff154a47eff41969346dd222846e0c7cbf5
tonyg <aj.gianno@gmail.com>
2021-04-27 14:33:17 -0500

    google auth login

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { MyOrdersComponent } from './../my-orders/my-orders.component';
 import { AdminOrdersComponent } from './../admin/admin-orders/admin-orders.component';
 import { AdminProductsComponent } from './../admin/admin-products/admin-products.component';
-// import { LoginComponent } from './../login/login.component';
+import { LoginComponent } from './../login/login.component';
 import { OrderSuccessComponent } from './../order-success/order-success.component';
 import { CheckOutComponent } from './../check-out/check-out.component';
 import { ShoppingCartComponent } from './../shopping-cart/shopping-cart.component';
@@ -17,7 +17,7 @@ const routes: Routes = [
   { path: 'check-out', component: CheckOutComponent },
   { path: 'my-orders', component: MyOrdersComponent },
   { path: 'order-success', component: OrderSuccessComponent },
-  // { path: 'login', component: LoginComponent },
+  { path: 'login', component: LoginComponent },
   { path: 'admin/admin-products', component: AdminProductsComponent },
   { path: 'admin/admin-orders', component: AdminOrdersComponent },
 ];
