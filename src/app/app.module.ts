690d0f78e3bacb2b29554e5c178762b1b49d2cb2
tonyg <aj.gianno@gmail.com>
2021-04-26 21:31:05 -0500

    routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireDatabaseModule } from '@angular/fire/database';
 import { AngularFireAuthModule } from '@angular/fire/auth';
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing/app-routing.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
+import { AppComponent } from './app.component';
+import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
+
+import { AppRoutingModule } from './app-routing/app-routing.module';
+
 @NgModule({
-  declarations: [],
+  declarations: [AppComponent, BsNavbarComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
     AngularFireAuthModule,
     NgbModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,8 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  title: String = 'Home!';
-
   constructor() {}
 
   ngOnInit(): void {}
